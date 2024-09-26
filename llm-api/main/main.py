30c87804127db6ff56e3975b2c9dfa86958fc9fa
MaximeR12 <122443585+MaximeR12@users.noreply.github.com>
2024-09-26 20:46:18 +0200

    api deployment done

--- llm-api/main/main.py
+++ llm-api/main/main.py
@@ -7,9 +7,10 @@ from fastapi import FastAPI, Security, Response, HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel
 import logging
+import time
 
 TOKENS_FILE = "../tokens.json"
-OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3:8b")
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3.2:3b")
 DB_API_URL = os.getenv("DB_API_URL", "http://localhost:8000")
 DB_API_TOKEN = os.getenv("DB_API_TOKEN")
 
@@ -70,31 +71,33 @@ def authenticate(credentials: HTTPAuthorizationCredentials = Security(security))
 
 @app.post("/article_analyse")
 async def ollama_chat(article: Script, language: str = "ENG", user_id: str = Depends(authenticate)):
+    start_time = time.time()  # Start the timer
     try:
         logger.debug(f"Received request for article analysis. Language: {language}, User ID: {user_id}")
         logger.debug(f"Article content: {article.script[:100]}...")  # Log first 100 characters of the article
-        
+        logger.info(f"used model: {OLLAMA_MODEL}")
         output = ollama.chat(
             model=OLLAMA_MODEL,
             messages=[{'role': 'user', 'content': f"""Context: You are an article analysis assistant. Your task is to provide a synthetic version of the input article and translate it if needed.
 
-            1. Summarize the following article in a concise manner, capturing the main points and key information:
-            {article.script}
+            Summarize the main points of the following article in 3-5 bullet points and translate into {language}. Provide only the translated bullet points. No additional text.
 
-            2. If the requested language is different from the article's original language, translate the summary to {language}.
-
-            Please provide the synthetic version (and translation if applicable) of the article."""
+            Article: {article.script}."""
             }]
         )
         logger.debug(f"Ollama chat response received. Content length: {len(output['message']['content'])}")
         output = output['message']['content']
+        response_time = time.time() - start_time  # Calculate the elapsed time
         log_data = {
             "model": OLLAMA_MODEL,
             "input": article.script,
             "output": output,
-            "response_time": "0:00:05.123456"
+            "response_time": f"{response_time:.6f}"  # Log the response time
         }
-        send_logs_to_db(log_data)
+        try:
+            send_logs_to_db(log_data)
+        except Exception as e:
+            logger.exception(f"Error sending logs to db: {str(e)}")
         return output
     except Exception as e:
         logger.exception(f"Error processing request: {str(e)}")
